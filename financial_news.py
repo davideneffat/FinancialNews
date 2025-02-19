3b0c59345a416d5900f935669ab291832b8dba39
u487241 <davide.neffat@intesasanpaolo.com>
2025-02-19 14:16:56 +0100

    First commit: try huggingface transformers and base model for summarization

---
+++ financial_news.py
@@ -0,0 +1,22 @@
+from transformers import pipeline
+import os
+os.environ["http_proxy"] = "http://inet1.gtm.corp.sanpaoloimi.com:9090/"
+os.environ["https_proxy"] = "http://inet1.gtm.corp.sanpaoloimi.com:9090/"
+
+
+def riassumi_transformers_astrattivo(testo):
+    """Riassume un testo utilizzando un modello pre-allenato di Transformers (summarization)."""
+    summarizer = pipeline("summarization") # Usa il modello predefinito per summarization
+    riassunto = summarizer(testo, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+    return riassunto
+
+testo_esempio = """
+La Banca Centrale Europea (BCE) ha deciso di alzare i tassi di interesse di 0.50 punti percentuali.
+Questa mossa è stata fatta per combattere l'inflazione elevata nella zona euro.
+La presidente della BCE, Christine Lagarde, ha dichiarato che ulteriori aumenti dei tassi potrebbero essere necessari.
+Gli analisti finanziari si aspettano che l'inflazione rimanga alta per il resto dell'anno.
+L'aumento dei tassi di interesse potrebbe avere un impatto sui mutui e sui prestiti per le imprese.
+"""
+
+riassunto_transformers = riassumi_transformers_astrattivo(testo_esempio)
+print(riassunto_transformers)
